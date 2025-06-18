f5638b89125e76020539c932cc87fd9ca2b54c53
Diego Vilhalva <diegorocha450@yahoo.com>
2025-06-18 11:58:41 -0300

    navbar created

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,27 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router"
+import { ClerkProvider } from "@clerk/clerk-react"
+import { ptBR } from "@clerk/localizations"
+import { dark } from '@clerk/themes'
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} localization={ptBR} appearance={{
+    baseTheme: dark,
+    variables: {
+      colorPrimary: '#f84565',
+      colorText: '#ffffff',
+      colorTextOnPrimaryBackground: "#ffffff",
+    }
+
+  }}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ClerkProvider>
 
 )
